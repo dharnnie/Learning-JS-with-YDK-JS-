efd0bc0d903887138635dd64d4215cdd3abbada8
Daniel Osineye <daniel.osineye@gmail.com>
2017-07-28 14:23:23 +0100

    Practice 1 done!

---
+++ code/js/practice1.js
@@ -0,0 +1,41 @@
+/*
+    Practice - Chapter 01 Page 36 | You don't know JS by Karl Max
+    This trial uses pre-defined constants [TAX_RATE, PHONE_PRICE, ACCESORY_PRICE, SPENDING_THRESHOLD]
+    
+    The other file "practice1-1.js" allows you to recieve input... Took the extra step like Karl adviced
+    Manipulate the spending treshold value to get different results or just run the upgraded file "practice1-1.js" to use your values
+
+    Navigate to the folder that has this file and run "node practice1.js"
+    - Daniel Osineye
+*/
+const TAX_RATE = 0.05;
+const PHONE_PRICE = 150000;
+const ACCESORY_PRICE = 15000;
+const SPENDING_THRESHOLD = 20000;
+var acctBalance = 1000000;
+
+var tax = function(cost) {
+    return TAX_RATE * cost;
+}
+
+// buy is synonymous to payment | It subtracts total amount you are spending(including accesories ... if applicable) from your  bank account! Spend wisely
+function buy(amt) {
+    console.log("Balance is :  ", acctBalance);
+    acctBalance = acctBalance - (amt + tax(amt));
+    console.log("Balance is :  ", acctBalance);
+}
+
+function purchase() {
+    var totalPurchase = PHONE_PRICE + ACCESORY_PRICE;
+    while (totalPurchase < acctBalance) {
+        if (totalPurchase < SPENDING_THRESHOLD) {
+            buy(totalPurchase);
+            console.log("You have purchased a phone with accesories");
+        } else {
+            console.log("You have reached your spending threshold, you can't buy any accesories");
+            buy(PHONE_PRICE);
+        }
+    }
+}
+
+purchase();
