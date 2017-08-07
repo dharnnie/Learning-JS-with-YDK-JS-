8c3e1719d45649ee49335bcb298813d5e1b24b81
Daniel Osineye <daniel.osineye@gmail.com>
2017-08-07 02:52:15 +0100

    RHS & LHS comments

--- code/js/scopes01.js
+++ code/js/scopes01.js
@@ -7,3 +7,12 @@ function foo(a) { // RHS look-up for foo LHS look-up for a
 }
 
 var c = foo(2); // LHS look-up for c --- RHS look-up for foo
+
+/*
+    Basically, RHS (Right hand side) Look-ups happens when a refrence is being made to a variable
+        Does [referenced dude] exist? We are safe, else, we are dead
+    On the other hand LHS (Left hand side) Look-ups happens when a declaration is being made
+        Has this been done before (In this scope?), No? -> Do it. else, ignore... look elsewhere
+
+        NB>: This is not all there is to RHS and LHS, this would definietely help 
+ */
